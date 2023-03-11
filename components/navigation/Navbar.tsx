f04faaf2d029ffeb036c91dcfa210079e0013052
unknown <robelin.alexandre71@gmail.com>
2023-03-11 20:23:49 +0100

    Reworked the component

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -14,13 +14,14 @@ import { getMessaging, onMessage } from "firebase/messaging";
 import { NotificationObject } from "@/Utils/interfaces";
 import NotificationBox from "./NotificationBox";
 import NavigationMenu from "./NavigationMenu";
+import Image from "next/image";
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useAtom(menuAtom);
   const [token, setJWTOKEN] = useAtom(jwtoken);
   const [_, setSearchPosts] = useAtom(atomPosts);
   const [searchBoolean, setSearchBoolean] = useState<boolean>(false);
-  const [searchBarInput, setSearchBarInput] = useState<any>();
+  const [searchBarInput, setSearchBarInput] = useState<string>();
   const [showNotification, setShowNotification] = useState<boolean>(false);
   const [localStorageNotifications, setLocalStorageNotifications] =
     useAtom(notifications);
@@ -30,6 +31,21 @@ export default function Navbar() {
   );
   function handleMenu() {
     setShowMenu(false);
+  }
+  function handleInBoxNotifications() {
+    const seenNotif = localStorageNotifications.map(
+      (a: NotificationObject, _: number): NotificationObject => {
+        return {
+          ...a,
+          data: {
+            ...a.data,
+            seen: "seen",
+          },
+        };
+      }
+    );
+    setLocalStorageNotifications(seenNotif);
+    setShowNotification((prev: boolean) => !prev);
   }
   useEffect(() => {
     (async () => {
@@ -77,38 +93,32 @@ export default function Navbar() {
                 <div>
                   <div
                     className="bell"
-                    style={{ display: "block" }}
-                    data-notif={
-                      unseenNotification.length > 1
-                        ? "http://localhost:3000/chat-svgrepo-com.svg"
-                        : ""
-                    }
+                    style={{ display: "block", position: "relative" }}
                   >
+                    {unseenNotification.length >= 1 ? (
+                      <Image
+                        src="/circle-moon-svgrepo-com.svg"
+                        alt="circle-moon-svgrepo-com.svg"
+                        width={10}
+                        height={10}
+                        style={{
+                          position: "absolute",
+                          zIndex: "20",
+                          right: "10px",
+                        }}
+                      />
+                    ) : (
+                      ""
+                    )}
                     <FontAwesomeIcon
                       className="svg-style bell"
+                      style={{ position: "relative", zIndex: "0" }}
                       icon={
                         localStorageNotifications.length > 0
                           ? faBell
                           : faBellSlash
                       }
-                      onClick={() => {
-                        const seenNotif = localStorageNotifications.map(
-                          (
-                            a: NotificationObject,
-                            _: number
-                          ): NotificationObject => {
-                            return {
-                              ...a,
-                              data: {
-                                ...a.data,
-                                seen: "seen",
-                              },
-                            };
-                          }
-                        );
-                        setLocalStorageNotifications(seenNotif);
-                        setShowNotification((prev: boolean) => !prev);
-                      }}
+                      onClick={handleInBoxNotifications}
                     ></FontAwesomeIcon>
                   </div>
                 </div>
