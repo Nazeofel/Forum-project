0c4a279f76319339499128715e603371242e4a85
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-03-15 21:12:01 +0100

    Db server update
    Fixing typing that caused deployment on vercel to crash

--- Utils/db.server.ts
+++ Utils/db.server.ts
@@ -1,16 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-let db: any;
+let primsa: PrismaClient;
 
 //check if we are running in production mode
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
+  primsa = new PrismaClient();
 } else {
-  //check if there is already a connection to the database
-  if (!global.db) {
-    global.db = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  db = global.db;
+  primsa = globalWithPrisma.prisma;
 }
 
-export { db };
+export { primsa };
